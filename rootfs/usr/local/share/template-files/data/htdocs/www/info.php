759ae4fea9d00ab01f64afac9c5e5d61fceafd11
casjay <git-admin@casjaysdev.com>
2023-03-14 23:40:52 -0400

    🗃️ Committing everything that changed 🗃️

--- rootfs/usr/local/share/template-files/data/htdocs/www/info.php
+++ rootfs/usr/local/share/template-files/data/htdocs/www/info.php
@@ -22,28 +22,43 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 -->
 
-  <meta charset="utf-8" />
-  <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-  <meta name="robots" content="index, follow" />
-  <meta name="generator" content="CasjaysDev" />
+    <meta charset="utf-8" />
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+    />
+    <meta name="robots" content="index, follow" />
+    <meta name="generator" content="CasjaysDev" />
 
-  <meta name="description" content="SERVER_SOFTWARE container" />
-  <meta property="og:title" content="SERVER_SOFTWARE container" />
-  <meta property="og:locale" content="en_US" />
-  <meta property="og:type" content="website" />
-  <meta property="og:image" content="/images/favicon.ico" />
-  <meta property="og:url" content="" />
+    <meta name="description" content="SERVER_SOFTWARE container" />
+    <meta property="og:title" content="SERVER_SOFTWARE container" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="./images/favicon.ico" />
+    <meta property="og:url" content="" />
 
-  <meta name="theme-color" content="#000000" />
-  <link rel="manifest" href="./site.webmanifest" />
+    <meta name="theme-color" content="#000000" />
+    <link rel="manifest" href="./site.webmanifest" />
 
     <link rel="icon" type="image/icon png" href="./images/favicon.png" />
-  <link rel="apple-touch-icon" href="/images/icon.png" />
-
-  <title>Welcome</title>
-</head>
+    <link rel="apple-touch-icon" href="./images/icon.png" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+    />
+    <link rel="stylesheet" type="text/css" href="./css/cookieconsent.css" />
+    <link rel="stylesheet" href="./css/bootstrap.css" />
+    <link rel="stylesheet" href="./css/index.css" />
+    <script src="./js/errorpages/isup.js"></script>
+    <script src="./js/errorpages/homepage.js"></script>
+    <script src="./js/errorpages/loaddomain.js"></script>
+    <script src="./js/jquery/default.js"></script>
+    <script src="./js/passprotect.min.js" defer></script>
+    <script src="./js/bootstrap.min.js" defer></script>
+    <script src="./js/app.js" defer></script>
+  </head>
 
 <body>
   <div class="container text-center">
