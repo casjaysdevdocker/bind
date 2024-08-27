75490387819feb36035aed4c676241890525faa7
casjay <git-admin@casjaysdev.pro>
2024-08-27 14:58:50 -0400

    🗃️ Committing everything that changed 🗃️
    rootfs/tmp/etc/bind/named.conf
    rootfs/usr/local/etc/docker/init.d/00-named.sh

--- rootfs/usr/local/etc/docker/init.d/00-named.sh
+++ rootfs/usr/local/etc/docker/init.d/00-named.sh
@@ -158,7 +158,7 @@ user_pass="${NAMED_USER_PASS_WORD:-}" # normal user password
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Additional predefined variables
 DNS_SERIAL="$(date +'%Y%m%d%S')"
-KEY_DHCP="${KEY_DHCP:-$(__dhcp_key || __tsig_key md5)}"
+KEY_DHCP="${KEY_DHCP:-$(__dhcp_key || __tsig_key sha256)}"
 KEY_RNDC="${KEY_RNDC:-$(__rndc_key || __tsig_key sha256)}"
 KEY_BACKUP="${KEY_BACKUP:-$(__backup_key || __tsig_key sha256)}"
 KEY_CERTBOT="${KEY_CERTBOT:-$(__certbot_key || __tsig_key sha512)}"
